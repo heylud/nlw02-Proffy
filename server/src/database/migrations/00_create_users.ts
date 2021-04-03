d5daf133a89e30e9f7be05802bca5fffb950810a
heylud <heylud@protonmail.com>
2021-04-02 21:33:22 -0300

    server

--- server/src/database/migrations/00_create_users.ts
+++ server/src/database/migrations/00_create_users.ts
@@ -1,6 +1,6 @@
 import Knex from 'knex';
 
-export async function up(knex: Knex) {
+export async function up(knex: Knex): Promise<void> {
 	return knex.schema.createTable('users', table => {
 		table.increments('id').primary();
 		table.string('name').notNullable();
@@ -9,6 +9,6 @@ export async function up(knex: Knex) {
 		table.string('bio').notNullable();
 	});
 }
-export async function down(knex: Knex) {
+export async function down(knex: Knex): Promise<void> {
 	return knex.schema.dropTable('users');
 }
